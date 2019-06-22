66ddfe35cfbd50f7327d1ce4df47d52e20236f18
Soyam Darshan Pattanaik <32511312+SoyamDarshan@users.noreply.github.com>
2019-06-22 23:19:46 +0530

    v1.1

--- src/index.js
+++ src/index.js
@@ -2,10 +2,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/*
+// Dynamic Function Example
+function getButtonText(){
+    return 'Click on me!';
+}
+*/
 // Create a react component.
 const App = () => {
-    return <div>Hi There!</div>;
-};
+    // Using a variable for assigning value.
+    const buttonText = {text: 'Click Me!'};
+    return (
+        <div>
+        <label className="label" htmlFor="name">Enter Name:</label>
+        <input id="name" type="text" />
+        <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        { buttonText.text }
+        </button>
+      </div>
+    );
+ };
 
 // Take the react component and show it on the screen.
 ReactDOM.render(
